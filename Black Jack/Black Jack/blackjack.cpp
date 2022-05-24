727cf7776aceee3b43d21de2f873ccd32b841894
Blake <blake.sarrett@gmail.com>
2022-05-24 07:51:51 -0500

    Implementation of Dealing first two cards, probably with memory leaks, and the first card seems to only deal an Ace of Hearts

--- Black Jack/Black Jack/blackjack.cpp
+++ Black Jack/Black Jack/blackjack.cpp
@@ -1,4 +1,17 @@
 #include "blackjack.h"
+#include <string>
+
+/* Various Card Constructors */
+blackjack::Card::Card(const Suite&& s, const Face_Value&& f) : suite(s), value(f) {};
+blackjack::Card::Card(const Suite& s, const Face_Value& f) : suite(s), value(f) {};
+blackjack::Card::Card() : suite(Suite::HEARTS), value(Face_Value::ACE) {}; // default values 
+
+/* To use std::cout << card */
+std::ostream& blackjack::operator << (std::ostream& out, const Card& c)
+{
+	out << Card::FACE_VALUE_NAMES[(int)c.value] << " of " << Card::SUITE_NAMES[(int)c.suite];
+	return out;
+}
 
 /* Constructor of Deck, loads deck unshuffled */
 blackjack::Deck::Deck(bool loadDeck)
@@ -11,9 +24,9 @@ blackjack::Deck::Deck(bool loadDeck)
 void blackjack::Deck::load_deck()
 {
 	int index = 0;
-	for (int i = 0; i < NUM_SUITES; i++)
+	for (int i = 0; i < Card::NUM_SUITES; i++)
 	{
-		for (int j = 0; j < NUM_FACE_VALUE; j++)
+		for (int j = 0; j < Card::NUM_FACE_VALUE; j++)
 		{
 			deck[index++] = Card((Suite)i, (Face_Value)j);
 		}
@@ -32,9 +45,9 @@ blackjack::Card& blackjack::Deck::operator [] (size_t index)
 }
 
 /* Constructor, allocate memory for the decks */
-blackjack::Dealer::Dealer(int numDecks) : num_decks(numDecks)
+blackjack::Dealer::Dealer(int nDecks) : numDecks(nDecks), cardIndex(0), deckIndex(0)
 {
-	card_deck = new Deck[num_decks];
+	card_deck = new Deck[numDecks];
 }
 
 /* Destructor, Free Dealer Resources */
@@ -57,16 +70,17 @@ void blackjack::Dealer::shuffle(int numShuffles)
 	{
 		for (int j = 0; j < Deck::NUM_CARDS; j++)
 		{
-			for (int k = 0; k < num_decks; k++)
+			for (int k = 0; k < numDecks; k++)
 			{
 				randomCardIndex = j + (rand() % (Deck::NUM_CARDS - j));
-				randomDeckIndex = k + (rand() % (num_decks - k));
+				randomDeckIndex = k + (rand() % (numDecks - k));
 				swap_cards(card_deck[randomDeckIndex][randomCardIndex], card_deck[k][j]);
 			}
 		}
 	}
 }
 
+/* Swap the two cards for the shuffle algorithm */
 void blackjack::Dealer::swap_cards(Card& a, Card& b)
 {
 	static Card temp; 
@@ -75,24 +89,38 @@ void blackjack::Dealer::swap_cards(Card& a, Card& b)
 	a = temp;
 }
 
+/* Deals one card */
+blackjack::Card blackjack::Dealer::deal_card()
+{
+	if (cardIndex >= Deck::NUM_CARDS)
+	{
+		cardIndex = 0;
+		deckIndex++; 
+	}
+
+	if (deckIndex >= numDecks)
+		throw std::runtime_error("ERROR: Ran out of cards\n");
+
+	return card_deck[cardIndex++][deckIndex];
+}
+
 /* Prints the deck to console */  
 std::ostream& blackjack::operator << (std::ostream& out, const blackjack::Dealer& d)
 {
-	Card card;
-	for (int i = 0; i < d.num_decks; i++)
+	for (int i = 0; i < d.numDecks; i++)
 	{
 		for (int j = 0; j < Deck::NUM_CARDS; j++)
 		{
-			card = d.card_deck[i][j];
-			out << Deck::FACE_VALUE_NAMES[(int)card.value] << " of " << Deck::SUITE_NAMES[(int)card.suite] << std::endl;
+			out << d.card_deck[i][j] << std::endl;
 		}
 	}
 	return out;
 }
 
+/* Retrieve the card with a deck and card index, throw error if an invalid index exists */
 const blackjack::Card& blackjack::Dealer::operator () (size_t deckIndex, size_t cardIndex) const
 {
-	if (deckIndex >= num_decks)
+	if (deckIndex >= numDecks)
 	{
 		throw std::runtime_error("ERROR: Invalid Deck Index using Dealer () operator\n");
 	}
@@ -105,6 +133,72 @@ const blackjack::Card& blackjack::Dealer::operator () (size_t deckIndex, size_t 
 	return card_deck[deckIndex][cardIndex]; 
 }
 
+blackjack::CardNode::CardNode(const Card& c)
+{
+	nextCard = nullptr;
+	card = c; 
+}
+
+blackjack::CardNode::CardNode()
+{
+	nextCard = nullptr;
+}
+
+/* How to delete nodes? */
+blackjack::CardNode::~CardNode()
+{
+	
+}
+
+void blackjack::CardNode::add(const Card& c)
+{
+	nextCard = new CardNode(c);
+}
+
+blackjack::Hand::Hand()
+{
+	listOfCards = nullptr;
+}
+
+/* How to delete list */
+blackjack::Hand::~Hand()
+{
+	if (listOfCards != nullptr)
+		delete listOfCards;
+}
+
+void blackjack::Hand::draw_card(const Card& c)
+{
+	if (listOfCards == nullptr)
+		throw std::runtime_error("ERROR: Must call init_hand() first\n");
+	listOfCards->add(c);
+}
+
+void blackjack::Hand::init_hand(const Card& c1, const Card& c2)
+{
+	listOfCards = new CardNode; 
+	listOfCards->add(c1);
+	listOfCards->add(c2); 
+}
+
+void blackjack::Hand::show_hand()
+{
+	std::cout << "Your Hand is: \n";
+	CardNode* temp = listOfCards;
+	do
+	{
+		std::cout << temp->card << std::endl;
+		temp = temp->nextCard;
+	} while (temp != nullptr);
+}
+
+blackjack::Player::Player() {};
+void blackjack::Player::set_hand(Card& c1, Card& c2)
+{
+	hand.init_hand(c1, c2); 
+}
+
+/* Test to see if every card is still in the deck and did not get lost somehow */
 blackjack::tester::Test_Result<blackjack::Card> blackjack::tester::test_shuffle(const Dealer& d)
 {
 
@@ -112,11 +206,11 @@ blackjack::tester::Test_Result<blackjack::Card> blackjack::tester::test_shuffle(
 	Card temp;
 	Deck tempDeck; // unshuffled deck
 	int count = 0;
-	int count_goal = d.numDecks();
+	int count_goal = d.num_decks();
 	for (int i = 0; i < Deck::NUM_CARDS; i++)
 	{
 		card_search = tempDeck[i]; 
-		for (int j = 0; j < d.numDecks(); j++)
+		for (int j = 0; j < d.num_decks(); j++)
 		{
 			for (int k = 0; k < Deck::NUM_CARDS; k++)
 			{
@@ -138,10 +232,15 @@ blackjack::tester::Test_Result<blackjack::Card> blackjack::tester::test_shuffle(
 /* Application */
 void blackjack::play()
 {
-	const int NUM_DECKS = 8; 
+	const int NUM_DECKS = 8; // Vegas usually uses 6-8 decks 
 	Dealer dealer(NUM_DECKS);
 	dealer.shuffle(); 
+
+
 	std::cout << "----- SHUFFLED DECK -----\n\n" << dealer << std::endl;
+
+	/*
+	// Tester Code 
 	std::cout << "----- SHUFFLE TEST ------\n\n";
 	auto result = tester::test_shuffle(dealer);
 	if (result.bSuccess)
@@ -156,6 +255,17 @@ void blackjack::play()
 					+ std::string(Deck::SUITE_NAMES[((int)result.data.suite)]) 
 			<< std::endl;
 	}
+	*/ 
+
+	Player player; 
+	Card firstTwo[2]; 
+
+	for (int i = 0; i < 2; i++)
+		firstTwo[i] = dealer.deal_card(); 
+
+	player.set_hand(firstTwo[0], firstTwo[1]); 
+	player.show_hand(); 
+
 	
 
 }
--- Black Jack/Black Jack/blackjack.h
+++ Black Jack/Black Jack/blackjack.h
@@ -30,25 +30,29 @@ namespace blackjack
 		KING = 12
 	};
 
-	struct Card
+	class Card
 	{
+	public:
+		Card(const Suite&& s, const Face_Value&& f);
+		Card(const Suite& s, const Face_Value& f);
+		Card();
 		Suite suite;
 		Face_Value value;
-		Card(const Suite&& s, const Face_Value&& f) : suite(s), value(f) {}; 
-		Card(const Suite& s, const Face_Value& f) : suite(s), value(f) {}; 
-		Card() : suite(Suite::HEARTS), value(Face_Value::ACE) {}; // default values 
+		static const int NUM_SUITES = 4;
+		static const int NUM_FACE_VALUE = 13;
+		static constexpr const char* SUITE_NAMES[NUM_SUITES] = { "Hearts", "Diamonds", "Spades", "Clubs" };
+		static constexpr const char* FACE_VALUE_NAMES[NUM_FACE_VALUE] = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+		friend std::ostream& operator << (std::ostream& out, const Card& c);
 	};
+
+	std::ostream& operator << (std::ostream& out, const Card& c);
 
 	class Deck
 	{
 	public: 
 		Deck(bool loadDeck = true);
 		Card& operator [] (size_t index);
-		static const int NUM_CARDS = 52;
-		static const int NUM_SUITES = 4;
-		static const int NUM_FACE_VALUE = 13;
-		static constexpr const char* SUITE_NAMES[NUM_SUITES] = { "Hearts", "Diamonds", "Spades", "Clubs" };
-		static constexpr const char* FACE_VALUE_NAMES[NUM_FACE_VALUE] = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+		static const int NUM_CARDS = 52; 
 
 	private:
 		Card deck[NUM_CARDS];
@@ -58,21 +62,69 @@ namespace blackjack
 	class Dealer
 	{
 	public:
-		Dealer(int numDecks = 1);
+		Dealer(int nDecks = 1);
 		~Dealer(); 
 		void shuffle(int numShuffles = 5); 
+		Card deal_card();
 		friend std::ostream& operator << (std::ostream& out, const Dealer& d);
-		const int numDecks() const { return num_decks; }
-		const int totalCards() const { return Deck::NUM_CARDS * num_decks; }
+		const int num_decks() const { return numDecks; }
+		const int totalCards() const { return Deck::NUM_CARDS * numDecks; }
 		const Card& operator () (size_t deckIndex, size_t cardIndex) const ; 
 
 	private:
-		int num_decks; 
+		int numDecks;
+		int cardIndex; 
+		int deckIndex;
 		Deck* card_deck;
 		void swap_cards(Card& a, Card& b);
 	};
 
 	std::ostream& operator << (std::ostream& out, const Dealer& d);
+
+	/* A Hand is a linked list of Cards
+	* I could have used std::list or std::vector 
+	* but I want some practice coding some data structures 
+	*/
+	class CardNode
+	{
+	public:
+		CardNode();
+		~CardNode();
+		void add(const Card& c);
+
+		Card card; 
+		CardNode* nextCard;
+
+	private:
+		// internal functionality
+		CardNode(const Card& c);
+
+	};
+
+	class Hand
+	{
+	public:
+		Hand();  
+		~Hand(); 
+		void show_hand(); 
+		void init_hand(const Card& c1, const Card& c2);
+		void draw_card(const Card& c); 
+		
+	private:
+		CardNode* listOfCards;
+	};
+
+	class Player
+	{
+	public:
+
+		Player(); 
+		void set_hand(Card& c1, Card& c2);
+		void show_hand() { hand.show_hand(); }
+
+	private:
+		Hand hand; 
+	};
 
 	namespace tester
 	{
@@ -87,6 +139,8 @@ namespace blackjack
 	}
 
 	void play(); 
+	void init_game(); 
+
 }
 
 #endif 
